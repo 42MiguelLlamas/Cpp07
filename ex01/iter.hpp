8fe515aea3efff3a5d1af5047f91c8b43e4b474e
MiguelLlamas <63542440+42MiguelLlamas@users.noreply.github.com>
2024-10-10 12:45:14 +0000

    ex00 hecho, on ex01...

---
+++ ex01/iter.hpp
@@ -0,0 +1,17 @@
+#ifndef ITER_HPP
+#define ITER_HPP
+
+#include <iostream>
+#include <string>
+
+template <typename T>
+void  iter (T &address, T &length, T &f)
+{
+    T c;
+    c = a;
+    a = b;
+    b = c;
+}
+
+
+#endif
